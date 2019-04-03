74d8cf65191f5c61fd5cbf92207a3e9834819c1b
Jaron <jkkoelewijn@live.nl>
2019-04-03 15:47:02 +0200

    Merge 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { RegisterPageComponent } from './register-page/register-page.component';
-
-const routes: Routes = [
-  {path: 'register-page', component: RegisterPageComponent}
-=======
 import {LoginPageComponent} from './login-page/login-page.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: '/login-page' , pathMatch: 'full'},
-  {path: 'login-page', component: LoginPageComponent}
->>>>>>> 753333462853fa23ae145927602d623211b4e303
+  {path: 'login-page', component: LoginPageComponent},
+  {path: 'register-page', component: RegisterPageComponent}
 ];
 
 @NgModule({
